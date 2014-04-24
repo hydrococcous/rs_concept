8b1c1720fca3cc8c40ac664880032085abcf0b16
Sandro Birke <sandro.birke@gmail.com>
2014-04-24 16:58:12 +0200

    handleDropEvent

--- js/function.js
+++ js/function.js
@@ -5,16 +5,33 @@ $(document).ready(function(){
 		$('.row-offcanvas').toggleClass('active');
 	});
 	
-	
-	
-	
 });
+
 
 $(init);
 
 function init(){
-	$('#makeMeDraggable').draggable();
+	$('#makeMeDraggable').draggable({
+		containment:'#content',
+		cursor:'move',
+		snap:'#makeMeDroppable',
+		revert: true
+	});
+	
+	$('#makeMeDroppable').droppable({
+		drop: handleDropEvent
+	});
 } 
 
+function handleDropEvent(event, ui){
+	var draggable = ui.draggable;
+	ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
+	ui.draggable.draggable( 'option', 'revert', false );
+}
 
 
+$('#makeMeDraggable').on('drag', function(event, ui){
+	
+	console.log('google jquery ui drag drop drag again);
+	
+});
